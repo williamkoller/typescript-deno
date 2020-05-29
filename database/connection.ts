6fc19b9f94715a18093ebb4c10d8f2496ba255ec
William Koller <wkoller25@gmail.com>
2020-05-29 01:33:00 -0300

    feat: add project on deno

---
+++ database/connection.ts
@@ -0,0 +1,34 @@
+import "https://deno.land/x/dotenv/load.ts"
+import { init, MongoClient } from "https://deno.land/x/mongo@v0.6.0/mod.ts"
+
+await init();
+
+const dataBase = Deno.env.get('DATABASE_NAME') || "deno"
+const dataBaseURI = Deno.env.get('DATABASE_HOST') || "mongodb://localhost:27017"
+
+class DataBase {
+  public client: MongoClient
+  constructor(
+    public dataBase: string,
+    public url: string
+  ) {
+    this.dataBase = dataBase
+    this.url = url
+    this.client = {} as MongoClient
+  }
+  
+  connect() {
+    const client = new MongoClient()
+    client.connectWithUri(this.url)
+    this.client = client
+  }
+  
+  get findDataBase() {
+    return this.client.database(this.dataBase)
+  }
+} 
+
+const connection = new DataBase(dataBase, dataBaseURI)
+connection.connect()
+
+export default connection
---
+++ server.ts
@@ -0,0 +1,24 @@
+import { Application } from "https://deno.land/x/abc@v1.0.0-rc2/mod.ts"
+import { ErrorMiddleware } from "./utils/handleError.ts"
+
+import {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser
+} from "./controller/User"
+
+const app = new Application()
+
+app.use(ErrorMiddleware)
+
+app
+.get("/allusers", getAllUsers)
+  .post("/create", createUser)
+  .get("/user/:id", getUser)
+  .put("/user/:id", updateUser)
+  .delete("/user/:id", deleteUser)
+  .start({ port: 4000 })
+
+console.log(`server listening on http://localhost:4000`);
